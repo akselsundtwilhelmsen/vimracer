2655e20efb604a6041f861e14363791316c2f65a
brag-v <brage.vik@lyse.net>
2023-03-23 11:58:25 +0100

    endret cursor-validitet og posisjon

--- app/src/main/java/vimracer/Vim.java
+++ app/src/main/java/vimracer/Vim.java
@@ -1,9 +1,6 @@
 package vimracer;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-
-import javafx.print.PrinterAttributes;
 import javafx.scene.input.KeyEvent;
 
 public class Vim extends TextWindow{
@@ -29,6 +26,7 @@ public class Vim extends TextWindow{
 
         if (keyString == "ESCAPE") {
             mode = 'n';
+            if (!validCursorPos(cursor)) cursor[0]--;
             return;
         }
 
@@ -43,20 +41,21 @@ public class Vim extends TextWindow{
             } else {
                 insertString(keyString);
             }
+            System.out.println(String.format("%d %d",cursor[0],cursor[1]));
             return;
         }
 
         switch (keyString) {
-            case "A":
+            case "a":
                 insertString("a");
                 break;
-            case "B":
+            case "b":
                 insertString("b");
                 break;
-            case "X":
+            case "x":
                 removeUnderCursor();
+            System.out.println(String.format("%d %d",cursor[0],cursor[1]));
         }
-        System.out.println(String.format("%d %d",cursor[0],cursor[1]));
     }
 
     public void keyRelease(KeyEvent event) {
@@ -67,7 +66,7 @@ public class Vim extends TextWindow{
     }
 
     private void moveCursor(int[] pos) {
-        if (pos.length != 2) throw new IllegalArgumentException();
+        if (!validCursorPos(pos)) throw new IllegalArgumentException();
         cursor = pos;
     }
 
@@ -78,8 +77,8 @@ public class Vim extends TextWindow{
 
     private void removeUnderCursor() {
         if (lines.get(cursor[1]).length() == 0) return;
-        lines.set(cursor[1], lines.get(cursor[1]).substring(0,cursor[0]-1) + lines.get(cursor[1]).substring(cursor[0]));
-        if (cursor[0] == lines.get(cursor[1]).length()+1) {
+        lines.set(cursor[1], lines.get(cursor[1]).substring(0,cursor[0]) + lines.get(cursor[1]).substring(cursor[0]+1));
+        if (! validCursorPos(cursor)) {
             cursor[0]--; 
         }
     }
@@ -89,4 +88,15 @@ public class Vim extends TextWindow{
         cursor[0]--;
         removeUnderCursor();
     }
+
+    private boolean validCursorPos(int[] pos) {
+        int currentLength = lines.get(cursor[1]).length();
+        if (pos.length != 2) return false;
+        if (0 > cursor[1] || cursor[1] >= lines.size()) return false;
+        if (cursor[0] < 0) return false;
+        if (mode == 'n' && currentLength != 0 && currentLength <= cursor[0]) return false;
+        if (currentLength < cursor[0]) return false;
+        return true;
+    }
+
 }
