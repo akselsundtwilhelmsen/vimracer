a7c2f2854bb025dfefde79b5f4b050f62d6748a5
Aksel Sundt Wilhelmsen <akselsundtwilhelmsen@gmail.com>
2023-03-24 10:41:48 +0100

    fikset tab-lengde i TextLoader.java

--- app/src/main/java/vimracer/TextLoader.java
+++ app/src/main/java/vimracer/TextLoader.java
@@ -11,20 +11,21 @@ public class TextLoader {
     public TextLoader() {
         lines = new ArrayList<>();
     }
-  public static void main(String[] args) {
-    try {
-      File myObj = new File("exampletext.txt");
-      Scanner myReader = new Scanner(myObj);
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        System.out.println(data);
-      }
-      myReader.close();
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
+
+    public static void main(String[] args) {
+        try {
+        File myObj = new File("exampletext.txt");
+        Scanner myReader = new Scanner(myObj);
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            System.out.println(data);
+        }
+        myReader.close();
+        } catch (FileNotFoundException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+        }
     }
-  }
 
     private void setDefaultText() {
         lines = new ArrayList<>();
