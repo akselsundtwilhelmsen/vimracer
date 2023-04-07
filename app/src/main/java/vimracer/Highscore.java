21c66fdd686b26d770ad7c36ad8a40660839ff84
Aksel Sundt Wilhelmsen <akselsundtwilhelmsen@gmail.com>
2023-04-07 17:01:46 +0200

    begynt på highscoresystem

--- app/src/main/java/vimracer/Highscore.java
+++ app/src/main/java/vimracer/Highscore.java
@@ -1,12 +1,36 @@
 package vimracer;
 
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Highscore {
+
+    private final String path = "src/main/resources/highscores.json";
     
     public Highscore() {
+    }
+
+    private void readFromFile() {
 
     }
 
-    public void writeToJson() {
+    private void writeToFile() {
 
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+            writer.write("ping");
+            writer.close();
+        }
+        catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Highscore a = new Highscore();
+        a.writeToFile();
     }
 }
