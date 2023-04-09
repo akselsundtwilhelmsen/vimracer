38debb24261d2f3b427a84b7e55ff6e924669cd3
Aksel Sundt Wilhelmsen <akselsundtwilhelmsen@gmail.com>
2023-04-09 19:22:03 +0200

    forsøk på å logge for brage

--- app/src/main/java/vimracer/TextLoader.java
+++ app/src/main/java/vimracer/TextLoader.java
@@ -45,8 +45,11 @@ public class TextLoader {
 
     public void listFiles(String promptPath) {
         File directory = new File(promptPath);
-        System.out.println(directory);
         File[] files = directory.listFiles();
+        System.out.println(directory);
+        System.out.println(directory.exists());
+        System.out.println(directory.isDirectory());
+        System.out.println(directory.listFiles());
         if (files != null) {
             for (int i = 0; i < files.length; i++) {
                 if (files[i].isFile()) {
