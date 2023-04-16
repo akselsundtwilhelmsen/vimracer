f16544631daf06a5a8a6be0e021cca6063c6bbe3
brag-v <brage.vik@lyse.net>
2023-04-16 21:40:18 +0200

    ferdig med tester

--- app/src/test/java/vimracer/VimTest.java
+++ app/src/test/java/vimracer/VimTest.java
@@ -1,6 +1,7 @@
 package vimracer;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,7 +21,7 @@ public class VimTest {
         vimtext.add("to");
         vimtext.add("tre");
         vimtext.add("");
-        vimtext.add("fem");
+        vimtext.add("fem that's great");
         vimtext.add("seks");
         vim.setText(vimtext);
     }
@@ -40,12 +41,45 @@ public class VimTest {
         Assertions.assertEquals(vim.toStringBetween(from, to),"null linjer");
 	}
 
-	// @Test
-	// @DisplayName("Test next regex")
-	// public void testNextInstanceOf() {
-    //     int[] from = {0,0,0,0};
-    //     int[] correctPos = {3,0,0,0};
-    //     Assertions.assertEquals(.wordEnd, correctPos);;
-	// }
+	@Test
+	@DisplayName("Test next regex")
+	public void testNextInstanceOf() {
+        //test inline
+        int[] from = {0,0,0,0}; 
+        int[] correctPos = {3,0,0,0};
+        Pattern regex = VimCommandList.WORDEnd;
+        Assertions.assertEquals(vim.nextInstanceOf(regex, from,false)[0],correctPos[0]);
 
+        correctPos[0] = 5;
+        regex = VimCommandList.WORDBeginning;
+        Assertions.assertEquals(vim.nextInstanceOf(regex, from,true)[0],correctPos[0]);
+
+        //test multiline
+        from[0] = 4;
+        from[1] = 1;
+        correctPos[0] = 11;
+        correctPos[1] = 5;
+        regex = Pattern.compile("great");
+        Assertions.assertEquals(vim.nextInstanceOf(regex, from,false)[0],correctPos[0]);
+        Assertions.assertEquals(vim.nextInstanceOf(regex, from,true)[1],correctPos[1]);
+	}
+
+	@Test
+	@DisplayName("Test previous regex")
+	public void testPrevInstanceOf() {
+        //test inline
+        int[] from = {6,1,0,0}; 
+        int[] correctPos = {3,1,0,0};
+        Pattern regex = VimCommandList.wordBeginning;
+        Assertions.assertEquals(vim.prevInstanceOf(regex, from,true)[0],correctPos[0]);
+
+        //test multiline
+        from[0] = 0;
+        from[1] = 6;
+        correctPos[0] = 3;
+        correctPos[1] = 1;
+        regex = Pattern.compile("en");
+        Assertions.assertEquals(vim.prevInstanceOf(regex, from,false)[0],correctPos[0]);
+        Assertions.assertEquals(vim.prevInstanceOf(regex, from,true)[1],correctPos[1]);
+	}
 }
